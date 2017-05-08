ec49b389b4b5f2bf30450412b6f510591b6223cf
Shubha Koundinyan <shubhakoundinyan@gmail.com>
2017-05-07 21:37:29 -0500

    Add files via upload

---
+++ ReadMe.txt
@@ -0,0 +1,20 @@
+  ____            _                   ____                            _   _             __  __             _ _             
+ / ___| _   _ ___| |_ ___ _ __ ___   |  _ \ _ __ ___  _ __   ___ _ __| |_(_) ___  ___  |  \/  | ___  _ __ (_) |_ ___  _ __ 
+ \___ \| | | / __| __/ _ \ '_ ` _ \  | |_) | '__/ _ \| '_ \ / _ \ '__| __| |/ _ \/ __| | |\/| |/ _ \| '_ \| | __/ _ \| '__|
+  ___) | |_| \__ \ ||  __/ | | | | | |  __/| | | (_) | |_) |  __/ |  | |_| |  __/\__ \ | |  | | (_) | | | | | || (_) | |   
+ |____/ \__, |___/\__\___|_| |_| |_| |_|   |_|  \___/| .__/ \___|_|   \__|_|\___||___/ |_|  |_|\___/|_| |_|_|\__\___/|_|   
+        |___/                                        |_|                                                                   
+
+OVERVIEW:
+
+The basic system properties that are monitored in this program are,
+
+1. RAM
+2. HARD DISK
+3. VIRTUAL MEMORY
+4. SWAP MEMORY
+5. CPU TEMPERATURE
+
+WHAT DOES THIS PROGRAM DO:
+
+A simple script that would help to check the above system properties. The output will be displayed on the screen as soon as the script is executed.
---
+++ sensor_info.py
@@ -0,0 +1,75 @@
+#! /usr/bin/python
+# encoding: utf-8
+
+import re
+import os
+import sys
+import platform
+import psutil
+import subprocess
+
+def lin_process():
+
+	process = psutil.Process(os.getpid())
+
+	# Displays the physical memory usage of the system
+
+	print "\n ########## PHYSICAL MEMORY USAGE ##########"
+	print "\n Memory Currently Used: "+str(process.memory_full_info().rss)
+	print "\n Percentage Memory Used: "+str(process.memory_percent())
+
+	# Displays the system disk utlization. USED vs. AVAILABLE
+
+	print "\n ########## DISK UTILIZATION for RESIDING FILE SYSTEM ##########"
+
+	bash_command_u = "df -h|grep /dev/disk1|awk '{print $3}' "
+	
+	print "\n Used Disk Space: "+str(subprocess.check_output(['bash', '-c', bash_command_u]))
+	
+	bash_command_a = "df -h|grep /dev/disk1|awk '{print $4}' "
+	
+	print " Available Disk Space: "+str(subprocess.check_output(['bash', '-c', bash_command_a]))
+
+	# Displays the virtual memory usage
+
+	print " ########## VIRTUAL MEMORY USAGE ##########"
+
+	print "\n Virtual Memory Used: "+str(psutil.virtual_memory().used)
+	print "\n Virtual Memory Available: "+str(psutil.virtual_memory().available)
+
+	print "\n ########## SWAP MEMORY USAGE ##########"	
+	
+	# Displays the swap memory usage
+
+	print "\n Swap Memory Used: "+str(psutil.swap_memory().used)
+	print "\n Swap Memory Available: "+str(psutil.swap_memory().free)
+	
+	'''
+	# This chunk of code that is commented out checks for the CPU temperature.
+	# It has a few pre-requisites that need to be in place before it can run successfully. 
+	
+	print "\n ########## CPU TEMPATURE CHECK ##########"
+	print psutil.sensors_temperatures()
+	if subprocess.check_output("sensors") ==1:
+		print "No sensors found or available!"
+	else:
+	
+		temperatures = {match[0]: float(match[1]) for match in re.findall("^(.*?)\:s+\+?(.*?)°C", sensors, re.MULTILINE)}
+		disk = "/dev/sda"
+		output = subprocess.check_output(["smartctl", "-A", disk])
+      	 	temperatures[disk] = int(re.search("Temperature.*\s(\d+)\s*(?:\([\d\s]*\)|)$", output, re.MULTILINE).group(1))
+		print "\nTemperature:"+str(temperatures[disk])
+		print "\nSensors temperature:"+str(psutil.sensors_temperatures())
+	'''
+	
+def main():
+
+	# Operating system and platform validation
+	
+	if platform.system()=="Linux" or platform.system()=="Darwin":
+		lin_process()
+	elif platform.system()=="Windows":
+		win_process()
+
+if __name__=="__main__":
+	main()
